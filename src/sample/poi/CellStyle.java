97eb5bc3d9abd3c9eba8e0a8542e45a44f053740
sundalei <sundalei2011@163.com>
2016-03-04 11:35:51 +0800

    cellstyle example

--- src/sample/poi/CellStyle.java
+++ src/sample/poi/CellStyle.java
@@ -3,6 +3,8 @@ package sample.poi;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
@@ -55,6 +57,51 @@ public class CellStyle {
 		cell.setCellStyle(style3);
 		
 		row = spreadsheet.createRow(8);
+		cell = row.createCell(3);
+		// Justified Alignment
+		XSSFCellStyle style4 = workbook.createCellStyle();
+		style4.setAlignment(XSSFCellStyle.ALIGN_JUSTIFY);
+		style4.setVerticalAlignment(XSSFCellStyle.VERTICAL_JUSTIFY);
+		cell.setCellValue("Contents are Justified in Alignment");
+		cell.setCellStyle(style4);
+		
+		// CELL BORDER
+		row = spreadsheet.createRow(10);
+		row.setHeight((short)800);
+		cell = row.createCell(1);
+		cell.setCellValue("BORDER");
+		
+		XSSFCellStyle style5 = workbook.createCellStyle();
+		style5.setBorderBottom(XSSFCellStyle.BORDER_THICK);
+		style5.setBottomBorderColor(IndexedColors.BLUE.getIndex());
+		style5.setBorderLeft(XSSFCellStyle.BORDER_DOUBLE);
+		style5.setLeftBorderColor(IndexedColors.GREEN.getIndex());
+		style5.setBorderRight(XSSFCellStyle.BORDER_HAIR);
+		style5.setRightBorderColor(IndexedColors.RED.getIndex());
+	    style5.setBorderTop(XSSFCellStyle.BIG_SPOTS);
+	    style5.setTopBorderColor(IndexedColors.CORAL.getIndex());
+	    cell.setCellStyle(style5);
+	    
+		// Fill Colors
+		// background color
+		row = spreadsheet.createRow((short) 12);
+		cell = (XSSFCell) row.createCell((short) 1);
+		XSSFCellStyle style6 = workbook.createCellStyle();
+		style6.setFillBackgroundColor(HSSFColor.LEMON_CHIFFON.index);
+		style6.setFillPattern(XSSFCellStyle.LESS_DOTS);
+		style6.setAlignment(XSSFCellStyle.ALIGN_FILL);
+		spreadsheet.setColumnWidth(1, 8000);
+		cell.setCellValue("FILL BACKGROUNG/FILL PATTERN");
+		cell.setCellStyle(style6);
+		// Foreground color
+		row = spreadsheet.createRow((short) 14);
+		cell = (XSSFCell) row.createCell((short) 1);
+		XSSFCellStyle style7 = workbook.createCellStyle();
+		style7.setFillForegroundColor(HSSFColor.BLUE.index);
+		style7.setFillPattern(XSSFCellStyle.LESS_DOTS);
+		style7.setAlignment(XSSFCellStyle.ALIGN_FILL);
+		cell.setCellValue("FILL FOREGROUND/FILL PATTERN");
+		cell.setCellStyle(style7);
 		
 		FileOutputStream out = new FileOutputStream(new File("cellstyle.xlsx"));
 		workbook.write(out);
